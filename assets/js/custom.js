888c50c7b1db6d09ee3992e07c9ea1a9570642b2
Dinesh <dineshwbd@gmail.com>
2024-09-01 18:38:16 +0530

    rel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,13 +6,24 @@ $(document).ready(function () {
         $(window).scroll(function () {
             if ($(this).scrollTop() > 800) {
                 $('#navbar_top').addClass("fixed-top");
+                $('#navbar_top').removeClass("navbar-light")
                 $('#navbar_top').removeClass("bg-transparent")
             } else {
                 $('#navbar_top').removeClass("fixed-top");
+                $('#navbar_top').addClass("navbar-light")
             }
         });
     }
 
     // AOS
     AOS.init();
+
+    $(function(){ 
+        var navLink = $("#offcanvasNavbar");
+        navLink.on("click", "a", null, function () {
+            navLink.collapse('hide') ;
+            $('.offcanvas-backdrop').hide();
+        });
+    });
 });
+
